b1f0dbc0eb6e1af1d34d1ece71820db9d1986023
EsraaMamoun <esraamamoun5@gmail.com>
2020-06-29 22:45:05 +0300

    deploy

---
+++ build/precache-manifest.046563f456b44cbb58f1833d57a0e4bf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1926d3abb91981715d149471515aa92a",
+    "url": "/deploy-resty/index.html"
+  },
+  {
+    "revision": "007bf12a0d0a9da7c69f",
+    "url": "/deploy-resty/static/css/main.26024cff.chunk.css"
+  },
+  {
+    "revision": "0b2184753d9be88d452d",
+    "url": "/deploy-resty/static/js/2.569d89fc.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/deploy-resty/static/js/2.569d89fc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "007bf12a0d0a9da7c69f",
+    "url": "/deploy-resty/static/js/main.8304d8f5.chunk.js"
+  },
+  {
+    "revision": "3bb7f4483697c9e575a4",
+    "url": "/deploy-resty/static/js/runtime-main.1b4cda75.js"
+  }
+]);
